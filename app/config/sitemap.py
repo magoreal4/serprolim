f0e5e441ef535e86ab9d001038d57937c4f7a81e
magoreal4 <magoreal4@gmail.com>
2021-12-06 11:31:17 -0400

    sitemap

--- app/config/sitemap.py
+++ app/config/sitemap.py
@@ -7,7 +7,7 @@ class Site:
     domain = 'limpiezapozossepticos.com'
 
 class PostSitemap(Sitemap):
-    changefreq = "daily"
+    changefreq = "weekly"
     priority = 0.8
  
     def get_urls(self, site=None, **kwargs):
